fc84cfbace58819fca2db6fb891b6e42d64a8d47
Nik Shlykov <nikshlykovdev@gmail.com>
2020-09-11 14:19:57 +0300

    add simple fragments classes

---
+++ app/src/main/java/ru/nikshlykov/donations/ui/fragments/DonationPageFragment.java
@@ -0,0 +1,21 @@
+package ru.nikshlykov.donations.ui.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import ru.nikshlykov.donations.R;
+
+public class DonationPageFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_donation_page, container, false);
+    }
+}
