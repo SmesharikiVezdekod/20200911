71c74fb163b1d481f61512f253307ef540a86517
Nik Shlykov <nikshlykovdev@gmail.com>
2020-09-11 13:10:39 +0300

    init

---
+++ app/src/main/java/ru/nikshlykov/donations/MainActivity.java
@@ -0,0 +1,21 @@
+package ru.nikshlykov.donations;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import ru.nikshlykov.donations.ui.main.MainFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow();
+        }
+    }
+}
---
+++ app/src/main/java/ru/nikshlykov/donations/ui/main/MainViewModel.java
@@ -0,0 +1,7 @@
+package ru.nikshlykov.donations.ui.main;
+
+import androidx.lifecycle.ViewModel;
+
+public class MainViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
