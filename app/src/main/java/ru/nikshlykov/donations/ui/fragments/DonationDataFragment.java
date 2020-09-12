06399157fe6a14e30d4aab3f37f193c1f83b16c7
ARTI1208 <leonardo906@mail.ru>
2020-09-12 10:46:54 +0300

    Data passing

--- app/src/main/java/ru/nikshlykov/donations/ui/fragments/DonationDataFragment.java
+++ app/src/main/java/ru/nikshlykov/donations/ui/fragments/DonationDataFragment.java
@@ -2,6 +2,7 @@ package ru.nikshlykov.donations.ui.fragments;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,17 +10,22 @@ import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.NavDirections;
 
 import ru.nikshlykov.donations.R;
+import ru.nikshlykov.donations.databinding.FragmentDonationDataBinding;
+import ru.nikshlykov.donations.model.DonationData;
 import ru.nikshlykov.donations.ui.OnFragmentInteractionListener;
+import ru.nikshlykov.donations.viewmodels.MainViewModel;
 
 public class DonationDataFragment extends Fragment {
 
     private OnFragmentInteractionListener onFragmentInteractionListener;
-
-    private String donationType;
+    private MainViewModel mViewModel;
+    private FragmentDonationDataBinding viewBinding;
 
     @Override
     public void onAttach(@NonNull Context context) {
@@ -30,39 +36,57 @@ public class DonationDataFragment extends Fragment {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        DonationDataFragmentArgs fragmentArgs = DonationDataFragmentArgs.fromBundle(getArguments());
-        donationType = fragmentArgs.getDonationType();
     }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_donation_data, container, false);
+        viewBinding = FragmentDonationDataBinding.inflate(inflater, container, false);
+        return viewBinding.getRoot();
     }
 
     @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider((AppCompatActivity) requireActivity()).get(MainViewModel.class);
 
-        NavDirections navDirections = null;
+        viewBinding.feeTitleEditText.setText(mViewModel.data.getTitle());
+        viewBinding.feeSumEditText.setText(String.valueOf(mViewModel.data.getSumRubles()));
+        viewBinding.feeAimEditText.setText(mViewModel.data.getAim());
+        viewBinding.feeDescriptionEditText.setText(mViewModel.data.getDescription());
+        viewBinding.feeAccountEditText.setText(mViewModel.data.getMoneyReceivingAccount());
+        viewBinding.feeAuthorEditText.setText(mViewModel.data.getAuthor());
 
-        Button nextOrCreateDonationButton = view.findViewById(R.id.fragment_donation_data___button___next_or_create_donation);
-
-        if (donationType.equals(getString(R.string.donation_type_target))) {
-            navDirections = DonationDataFragmentDirections.actionNavDonationDataToNavDonationOptionData();
-        } else if (donationType.equals(getString(R.string.donation_type_regular))) {
-            navDirections = DonationDataFragmentDirections.actionNavDonationDataToNavDonationPreview();
-            nextOrCreateDonationButton.setText(getString(R.string.create_donation));
-            view.findViewById(R.id.fragment_donation___linear_layout___author).setVisibility(View.VISIBLE);
-        }
-        final NavDirections finalNavDirections = navDirections;
-        nextOrCreateDonationButton.setOnClickListener(new View.OnClickListener() {
+        viewBinding.fragmentDonationDataButtonNextOrCreateDonation.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (finalNavDirections != null) {
-                    onFragmentInteractionListener.onFragmentInteraction(finalNavDirections);
+                NavDirections navDirection = null;
+
+                if (mViewModel.data.getType() == DonationData.TYPE_ONE_TIME) {
+                    navDirection = DonationDataFragmentDirections.actionNavDonationDataToNavDonationOptionData();
+                } else if (mViewModel.data.getType() == DonationData.TYPE_REPETITIVE) {
+                    navDirection = DonationDataFragmentDirections.actionNavDonationDataToNavDonationPreview();
+                }
+
+                if (navDirection != null) {
+                    mViewModel.data.setTitle(viewBinding.feeTitleEditText.getText().toString());
+                    mViewModel.data.setSumRubles(Double.parseDouble(viewBinding.feeSumEditText.getText().toString()));
+                    mViewModel.data.setAim(viewBinding.feeAimEditText.getText().toString());
+                    mViewModel.data.setDescription(viewBinding.feeDescriptionEditText.getText().toString());
+                    mViewModel.data.setMoneyReceivingAccount(viewBinding.feeAccountEditText.getText().toString());
+
+                    if (mViewModel.data.getType() == DonationData.TYPE_REPETITIVE) {
+                        mViewModel.data.setAuthor(viewBinding.feeAuthorEditText.getText().toString());
+                    }
+
+                    onFragmentInteractionListener.onFragmentInteraction(navDirection);
                 }
             }
         });
+
+        if (mViewModel.data.getType() == DonationData.TYPE_REPETITIVE) {
+            viewBinding.fragmentDonationDataButtonNextOrCreateDonation.setText(getString(R.string.create_donation));
+            viewBinding.fragmentDonationLinearLayoutAuthor.setVisibility(View.VISIBLE);
+        }
     }
 }
--- app/src/main/java/ru/nikshlykov/donations/viewmodels/MainViewModel.java
+++ app/src/main/java/ru/nikshlykov/donations/viewmodels/MainViewModel.java
@@ -2,6 +2,8 @@ package ru.nikshlykov.donations.viewmodels;
 
 import androidx.lifecycle.ViewModel;
 
+import ru.nikshlykov.donations.model.DonationData;
+
 public class MainViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
+    public DonationData data;
 }
